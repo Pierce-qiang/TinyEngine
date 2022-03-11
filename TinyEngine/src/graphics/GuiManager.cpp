09a44e0ebad3670d03a9f082effeaedc8649b267
Pierce_qiang <2579404335@qq.com>
2022-03-11 11:38:57 +0800

    finish basic logic for debug

--- TinyEngine/src/graphics/GuiManager.cpp
+++ TinyEngine/src/graphics/GuiManager.cpp
@@ -1,5 +1,6 @@
 #include "GuiManager.h"
 #include <unordered_map>
+
 namespace TEngine {
 	GuiManager::GuiManager(Scene* scene, RenderPassManager* renderPassManager) :mScene(scene), mRenderpassManager(renderPassManager) {
 #pragma region imgui
@@ -11,6 +12,7 @@ namespace TEngine {
 		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
 		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 		//io.ConfigViewportsNoAutoMerge = true;
 		//io.ConfigViewportsNoTaskBarIcon = true;
 
@@ -41,26 +43,77 @@ namespace TEngine {
 	}
 	void GuiManager::Draw() {
 		PrepareFrame();
-
 		ImGui::ShowDemoWindow();
 		//TODO: Write Gui logic------------------------
+
+#define DEBUG
+#ifdef DEBUG
+		//the input callback and the textinput do not decouple, I don't know how to do it
+#pragma region drawBufferTextures
 		auto outputBuffers = mRenderpassManager->GetFrameBuffers();
 		for (auto& item : outputBuffers) {
-			if ("Scene"==item.first)
-			{
-				item.second->GetColourTexture()->displayTexture(item.first);
-			}
-			else if("GeometryPass_Output"==item.first)
+			if ("GeometryPass_Output" == item.first)
 			{
 				GBuffer* gOutput = dynamic_cast<GBuffer*>(item.second);
 				assert(gOutput != nullptr);
 				auto& textures = gOutput->GetColourTextures();
-				
+				ImGui::Begin(item.first.c_str());
+				//save frame
+				bool savePictur = false;
+				ImGui::Checkbox("save picture", &savePictur);
+				//text box
+				static char name[64] = ""; ImGui::InputText("saveName", name, 64);
+				if (savePictur)
+				{
+					savePictur = false;
+					gOutput->SaverRenderFrame(name);
+				}
 
+				for (auto& texture : textures) {
+					texture->displayTexture();
+				}
+				ImGui::End();
 			}
+			else if ("ShadowMap_Output" == item.first) {
+				ImGui::Begin(item.first.c_str());
+				bool savePictur = false;
+				ImGui::Checkbox("save picture", &savePictur);
+				//text box
+				static char name[64] = ""; ImGui::InputText("saveName", name, 64);
+				if (savePictur)
+				{
+					savePictur = false;
+					item.second->SaverRenderFrame(name);
+				}
+				item.second->GetDepthStencilTexture()->displayTexture();
+				ImGui::End();
+			}
+			else
+			{
+				//create a new window
+				ImGui::Begin(item.first.c_str());
+				bool savePictur = false;
+				ImGui::Checkbox("save picture", &savePictur);
+				//text box
+				static char name[64] = ""; ImGui::InputText("saveName", name, 64);
+				if (savePictur)
+				{
+					savePictur = false;
+					item.second->SaverRenderFrame(name);
+				}
+				item.second->GetColourTexture()->displayTexture();
+				ImGui::End();
+			}
+
 		}
+#pragma endregion
+#endif // DEBUG
+		
+		//TODO: Write Scene Hierachy
 
 
+		
+		
 
 		EndFrame();
 	}
@@ -74,12 +127,10 @@ namespace TEngine {
 
 	void GuiManager::EndFrame() {
 #pragma region endFrame
+		ImGuiIO& io = ImGui::GetIO();
 		// Rendering
 		ImGui::Render();
-		int display_w, display_h;
 		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-		glfwGetFramebufferSize(WindowManager::Instance()->GetWindow(), &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
 		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
 		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -87,7 +138,6 @@ namespace TEngine {
 		// Update and Render additional Platform Windows
 		// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
 		//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
-		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
 			GLFWwindow* backup_current_context = glfwGetCurrentContext();
